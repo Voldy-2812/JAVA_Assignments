65a2e88db1e6cfb9389467c320198667f7068d03
Rudreshwar Baranwal <baranwalrudreshwar@gmail.com>
2022-03-02 03:50:01 +0530

    Create que1.java

---
+++ Assignment-1/que1.java
@@ -0,0 +1,11 @@
+class Message {
+    void display() {
+        System.out.println("Welcome to Java programming world\n");
+    }
+}
+public class que1 {
+    public static void main(String[] args) {
+        Message message1 = new Message();
+        message1.display();
+    }
+}
