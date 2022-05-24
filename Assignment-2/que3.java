01616e920bfd42b4e67a8e72e5287440bca5d532
Rudreshwar Baranwal <baranwalrudreshwar@gmail.com>
2022-05-24 06:43:30 +0530

    Create que3.java

---
+++ Assignment-2/que3.java
@@ -0,0 +1,8 @@
+public class que3 {
+    public static void main(String args[]) {
+        String str = args[0];
+        if((int)str.charAt(0) > 90 || (int)str.charAt(0) < 65) {
+            System.out.println("ERROR !!!!");
+        }
+    }
+}
